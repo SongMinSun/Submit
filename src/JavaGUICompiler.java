731307449dbebc50610220412f601512142a4367
JIJIClyde <eric3285@naver.com>
2021-05-07 10:02:44 +0900

    import, 뼈대 구축

--- src/JavaGUICompiler.java
+++ src/JavaGUICompiler.java
@@ -1,25 +1,54 @@
-import java.awt.EventQueue;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import javax.tools.*;
 
-import javax.swing.JFrame;
+public class JavaGUICompiler extends JFrame {
+	private static final long serialVersionUID = 1L;
+	StringBuffer buffer;
+	Process process;
+	BufferedReader bufferedReader;
+	StringBuffer readBuffer;
+	
+	JavaCompiler compiler;
+	Clipboard c;
+	
+	JFrame f = new JFrame();
+	JTextArea tf1 = new JTextArea();
+	JTextArea tf2 = new JTextArea();
+	
+	JScrollPane scrollTf1 = new JScrollPane(tf1);
+	JScrollPane scrollTf2 = new JScrollPane(tf2);
+	
+	JMenuBar mb = new JMenuBar();
+	JMenu fileMenu = new JMenu("File");
+	JMenu editMenu = new JMenu("Edit");
+	JMenu compileMenu = new JMenu("Compile");
 
-public class JavaGUICompiler {
-
-	private JFrame frame;
+	JFileChooser chooser;
+	JMenuItem NewMenu = new JMenuItem("New");
+	JMenuItem OpenMenu = new JMenuItem("Open");
+	JMenuItem SaveMenu = new JMenuItem("Save");
+	JMenuItem EditMenu = new JMenuItem("Edit");
+	
+	JMenuItem CopyMenu = new JMenuItem("Copy");
+	JMenuItem PasteMenu = new JMenuItem("Paste");
+	JMenuItem CutMenu = new JMenuItem("Cut");
+	
+	JMenuItem CompileMenu = new JMenuItem("Compile");
+	JMenuItem RunMenu = new JMenuItem("Run");
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					JavaGUICompiler window = new JavaGUICompiler();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		new JavaGUICompiler();
 	}
 
 	/**
@@ -33,9 +62,6 @@ public class JavaGUICompiler {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
