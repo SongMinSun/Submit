5f8fb8ed4ebe91e7636a09b026497314de5ddd10
SongMinSun <sms010117@naver.com>
2021-05-07 16:00:20 +0900

    action 틀& new, open action

--- src/JavaGUICompiler.java
+++ src/JavaGUICompiler.java
@@ -35,7 +35,7 @@ public class JavaGUICompiler extends JFrame {
 	JMenuItem NewMenu = new JMenuItem("New");
 	JMenuItem OpenMenu = new JMenuItem("Open");
 	JMenuItem SaveMenu = new JMenuItem("Save");
-	JMenuItem EditMenu = new JMenuItem("Edit");
+	JMenuItem ExitMenu = new JMenuItem("Exit");
 	
 	JMenuItem CopyMenu = new JMenuItem("Copy");
 	JMenuItem PasteMenu = new JMenuItem("Paste");
@@ -59,6 +59,8 @@ public class JavaGUICompiler extends JFrame {
 		this.setSize(500,500);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		CreateMenuBar();
+		CreateTextField();
 		this.setVisible(true);
 		initialize();
 	}
@@ -67,6 +69,102 @@ public class JavaGUICompiler extends JFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+	}
+	
+	public void CreateMenuBar() {
+		mb.add(fileMenu);
+		mb.add(editMenu);
+		mb.add(compileMenu);
+		
+		fileMenu.add(NewMenu);
+		fileMenu.add(OpenMenu);
+		fileMenu.add(SaveMenu);
+		fileMenu.add(ExitMenu);
+		
+		editMenu.add(CopyMenu);
+		editMenu.add(PasteMenu);
+		editMenu.add(CutMenu);
+		
+		compileMenu.add(CompileMenu);
+		compileMenu.add(RunMenu);
+		
+		NewMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tf1.setText("");
+				tf2.setText("");
+			}
+		});
+		
+		OpenMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				chooser = new JFileChooser();
+				int retval = chooser.showOpenDialog(null);
+				if(retval != JFileChooser.APPROVE_OPTION) {
+					JOptionPane.showMessageDialog(null, "파일을 선택하지 않았음.","경고",JOptionPane.WARNING_MESSAGE);
+					return;
+				}
+				File file = null;
+				if(retval == JFileChooser.APPROVE_OPTION) {
+					file = chooser.getSelectedFile();
+				}
+				BufferedReader br = null;
+				try {
+					br = new BufferedReader(new FileReader(file));
+					String line;
+					while((line = br.readLine()) != null) {
+						tf1.append(line + "\n");
+					}
+				}catch(Exception e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		
+		SaveMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		ExitMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		CopyMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		PasteMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		CutMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		CompileMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		RunMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		this.setJMenuBar(mb);
+		
+	}
+	
+	public void CreateTextField() {
+		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, scrollTf1, scrollTf2);
+		splitPane.setResizeWeight(0.5);
+		getContentPane().add(splitPane);
 	}
 
 }
